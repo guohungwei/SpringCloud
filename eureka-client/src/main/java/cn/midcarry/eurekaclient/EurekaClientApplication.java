9c5e9ef47493ba19ca823045e842f80864225245
hungweiguo <midcarry@qq.com>
2019-03-29 00:46:38 +0800

    '修改gitignore文件'

--- eureka-client/src/main/java/cn/midcarry/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/cn/midcarry/eurekaclient/EurekaClientApplication.java
@@ -22,6 +22,6 @@ public class EurekaClientApplication {
 
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "forezp") String name) {
-        return "hi " + name + " ,i am from port:" + port + ", i'm running~";
+        return "hi " + name + " ,i am from port:" + port ;
     }
 }
